1d44140e88dda78cbc67f27bfd298e7d6682dca0
Scott Sitar <scottsitar@gmail.com>
2023-09-23 21:15:22 -0700

    testing pages

---
+++ spelling/spelling.js
@@ -0,0 +1,57 @@
+var words = [
+    'today',
+    'tomorrow',
+    'yesterday',
+    'complicated',
+    'stairs',
+    'computer',
+    'mountain',
+]
+
+var word = words[0]
+var synth = window.speechSynthesis
+var utterance = new SpeechSynthesisUtterance(word)
+utterance.rate = 0.8
+utterance.lang = "en"
+
+var answerBox
+var showMeBox
+
+function initialize() {
+    answerBox = document.getElementById('answerbox')
+    showMeBox = document.getElementById('showmebox')
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function resetUI() {
+    answerBox.value = ''
+    showMeBox.innerHTML = ''
+}
+
+function nextWord() {
+    let i = getRandomInt(words.length)
+    word = words[i]
+    utterance.text = word
+    resetUI()
+    sayAgain()
+}
+
+function checkWord() {
+    let guess = answerBox.value
+    if (guess == word) {
+        alert('Right!')
+    } else {
+        alert('Wrong!')
+    }
+}
+
+function sayAgain() {
+    synth.speak(utterance)
+}
+
+function showMe() {
+    showMeBox.innerHTML = word
+}
