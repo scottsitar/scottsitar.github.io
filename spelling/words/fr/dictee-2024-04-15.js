d5eebe1c6cefe172bc1148f53d0784833a297e1f
Scott Sitar <scottsitar@gmail.com>
2024-04-16 19:43:35 -0700

    next list

---
+++ spelling/words/fr/dictee-2024-04-15.js
@@ -0,0 +1,12 @@
+export const words = [
+    'une invitation',
+    'chez',
+    'un jour de congé',
+    'la gauche',
+    'la droite',
+    'la rue',
+    'jusqu\'à',
+    'le perron',
+    'la direction',
+    'maintenant',
+]
