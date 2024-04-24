3d485645573a200d7c9f702779eb7dcd1854bc0f
Scott Sitar <scottsitar@gmail.com>
2024-04-23 19:01:56 -0700

    list words

--- spelling/words/fr/dictee-2024-04-22.js
+++ spelling/words/fr/dictee-2024-04-22.js
@@ -34,4 +34,15 @@ export const words = [
     'vous avez pu',
     'ils ont pu',
     'elles ont pu',
+
+    'étonnant',
+    'une bicyclette',
+    'une grimace',
+    'un outil',
+    'les trucs',
+    'une débarbouillette',
+    'un marteau',
+    'une cure-dent',
+    'gesticuler',
+    'un grognement',
 ]
