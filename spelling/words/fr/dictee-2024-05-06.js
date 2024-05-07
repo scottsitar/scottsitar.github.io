3ce1dab8ab5876b0aba5f08ddb4092119dece408
Scott Sitar <scottsitar@gmail.com>
2024-05-06 20:00:10 -0700

    here comes another one...

---
+++ spelling/words/fr/dictee-2024-05-06.js
@@ -0,0 +1,12 @@
+export const words = [
+    'la pluie',
+    'le grenier',
+    'l\'aventure',
+    'les indications',
+    'une heure',
+    'finalement',
+    'l\'intérieur',
+    'un pique-nique',
+    'déchirant',
+    'un souvenir',
+]
